3cba02d80b35f466ef713400b70385d86be11a43
Nihil16 <102133258+Nihil16@users.noreply.github.com>
2023-06-20 06:53:12 +1000

    v2.1.1

--- Assets/Scripts/Monsters/BottomHat.cs
+++ Assets/Scripts/Monsters/BottomHat.cs
@@ -24,19 +24,23 @@ public class BottomHat : MonoBehaviour
 
     private IEnumerator Rabbit(int Soloution)
     {
-        yield return new WaitForSeconds(1);
-        LeanTween.move(Hat, new Vector3(0, -2.135f, 0), 0.2f);
-        Hat.transform.parent = null;
-        yield return new WaitForSeconds(2);
-        for (int i = 0; i < Soloution; i++)
+        if (Hat != null)
         {
-            Instantiate(Spawnables[0], Spawnpoint.transform.position, quaternion.identity);
-            yield return new WaitForSeconds(1.2f);
-        }
-        
-       
 
-        
+
+            yield return new WaitForSeconds(1);
+            LeanTween.move(Hat, new Vector3(0, -2.135f, 0), 0.2f);
+            Hat.transform.parent = null;
+            yield return new WaitForSeconds(2);
+            for (int i = 0; i < Soloution; i++)
+            {
+                Instantiate(Spawnables[0], Spawnpoint.transform.position, quaternion.identity);
+                yield return new WaitForSeconds(1.2f);
+            }
+        }
+
+
+
 
 
     }
