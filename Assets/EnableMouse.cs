e2359254d50a85708738c974fa6ed269b15d0c77
Nihil16 <102133258+Nihil16@users.noreply.github.com>
2023-07-04 12:39:46 +1000

    v3.3

---
+++ Assets/EnableMouse.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableMouse : MonoBehaviour
+{
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = true;
+    }
+}
